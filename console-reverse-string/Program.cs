905c0348dac33d24db74246560834df26db2ada9
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-09-08 22:28:39 +0200

    add exercices

---
+++ console-reverse-string/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Write the code necessary to reverse the letters of each word in place and display the result.
+In other words, don't just reverse every letter in the variable pangram.
+Instead, you'll need to reverse just the letters in each word,
+    but print the reversed word in it's original position in the message.
+
+    string pangram = "The quick brown fox jumps over the lazy dog";
+*/
+using System;
+
+namespace console_reverse_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pangram = "The quick brown fox jumps over the lazy dog";
+            string[] words = pangram.Split(' ');
+            string[] newPangram = new string[words.Length];
+
+
+            for (int i = 0, size = words.Length; i < size; i++)
+            {
+                char[] word = words[i].ToCharArray();
+                Array.Reverse(word);
+                newPangram[i] = String.Join("", word);
+            }
+
+            Console.WriteLine("\nOriginal string: " + String.Join(" ",pangram));
+            Console.WriteLine("Reversed string: " + String.Join(" ",newPangram) + "\n");
+        }
+    }
+}
---
+++ console-rpg-duel/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace console_rpg_duel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heroLife = 10;
+            int monsterLife = 10;
+            int heroAttack = 0;
+            int monsterAttack = 0;
+            Random dice = new Random();
+
+            while (heroLife > 0 && monsterLife > 0)
+            {
+                Console.WriteLine($"\n{"Hero HP",-10}: {heroLife,2}\n{"Monster HP",-10}: {monsterLife,2}\n");
+
+                heroAttack = dice.Next(1, 11);
+                Console.WriteLine($"The hero attacks the monster and hit for {heroAttack,2} point{(heroAttack > 1 ? "s" : "")}");
+                monsterLife -= heroAttack;
+
+                if (monsterLife > 0)
+                {
+                    monsterAttack = dice.Next(1, 11);
+                    Console.WriteLine($"The monster attacks the hero and hit for {monsterAttack,2} point{(monsterAttack > 1 ? "s" : "")}");
+                    heroLife -= monsterAttack;
+                }
+            }
+            Console.WriteLine(heroLife > monsterLife ? "The hero has killed the monster\n" : "The monster has killed the hero\n");
+        }
+    }
+}
